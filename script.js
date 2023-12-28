930c6dc580885cbc81465de7e7bd157b2c55f574
Adrian Guereque <124836243+adrianguereque@users.noreply.github.com>
2023-12-28 00:42:37 -0600

    Changed from times to cards (not done)

--- script.js
+++ script.js
@@ -1,26 +1,10 @@
-const libraryShow = document.querySelector('.show');
-const libraryAdd = document.querySelector('.add-book');
-libraryAdd.style.display = "none";
+function openPopup() {
+    document.getElementById("overlay").style.display = "flex";
+}
 
-const toFormBtn = document.querySelector('.to-form-btn');
-const goBackBtn = document.querySelector('.go-back');
-
-toFormBtn.addEventListener('click', () =>{
-    libraryAdd.style.display = "initial";
-    libraryShow.style.display = "none";
-})
-
-goBackBtn.addEventListener('click', () => {
-    libraryAdd.style.display = "none";
-    libraryShow.style.display = "initial";
-})
-
-const bookContents = [
-    document.querySelector('.book-title'),
-    document.querySelector('.author'),
-    document.querySelector('.pages'),
-    document.querySelector('.status')
-]
+  function closePopup() {
+    document.getElementById("overlay").style.display = "none";
+}
 
 const theHobbit = new Book("Lord of the Rings","Tolkien",256,1);
 const DCC = new Book("Dungeon Crawler Carl","Aussie",345,1);
@@ -42,27 +26,3 @@ function addBookToLibrary() {
   // ask them through forms on webpage or smn
   myLibrary.push(new Book(title, author, pages, read));
 }
-
-// slide
-
-let i = 0;
-let time = 3000;
-
-function changeSlide(){
-    bookContents[0].textContent = myLibrary[i].title;
-    bookContents[1].textContent = myLibrary[i].author;
-    bookContents[2].textContent = myLibrary[i].pages;
-    bookContents[3].textContent = myLibrary[i].read;
-	if(i < myLibrary.length - 1){
-	  i++; 
-	} 
-    else { 
-		i = 0;
-	}
-
-	// Run function every x seconds
-	setTimeout("changeSlide()", time);
-}
-
-// Run function when page loads
-window.onload=changeSlide;
